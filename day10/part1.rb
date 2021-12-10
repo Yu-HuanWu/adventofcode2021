406cb284f798a6526981e51d3dec1bd58224fe6e
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-12-09 23:56:03 -0800

    part1 upload

--- day10/part1.rb
+++ day10/part1.rb
@@ -3,13 +3,61 @@ g= f.split(("\n"))
 
 g.map! {|str| str.split("")}
 
+illegal= []
 g.each do |str|
     tracking = []
     str.each do |char|
         if char == "("
             tracking << ")"
         elsif char == ")"
-            
+            if tracking[-1] == ")"
+                tracking.pop
+            else
+                illegal << char
+                break
+            end
+        elsif char == "["
+            tracking << "]"
+        elsif char == "]"
+            if tracking[-1] == "]"
+                tracking.pop
+            else
+                illegal << char
+                break
+            end
+        elsif char == "{"
+            tracking << "}"
+        elsif char == "}"
+            if tracking[-1] == "}"
+                tracking.pop
+            else
+                illegal << char
+                break
+            end
+        elsif char == "<"
+            tracking << ">"
+        elsif char == ">"
+            if tracking[-1] == ">"
+                tracking.pop
+            else
+                illegal << char
+                break
+            end
         end
     end
 end
+
+points= 0
+illegal.each do |char|
+    if char == ")"
+        points+=3
+    elsif char == "]"
+        points+=57
+    elsif char == "}"
+        points += 1197
+    elsif char == ">"
+        points += 25137
+    end
+end
+
+p points

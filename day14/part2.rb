821f5719571cc1db428f92b854c2969221e1e623
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-12-14 00:54:01 -0800

    deleted test case

--- day14/part2.rb
+++ day14/part2.rb
@@ -1,20 +1,4 @@
 f= File.read("input")
-# f="CH -> B
-# HH -> N
-# CB -> H
-# NH -> C
-# HB -> C
-# HC -> B
-# HN -> C
-# NN -> C
-# BH -> H
-# NC -> B
-# NB -> B
-# BN -> B
-# BB -> N
-# BC -> B
-# CC -> N
-# CN -> C"
 g= f.split(("\n"))
 h= []
 
@@ -28,7 +12,6 @@ h.each do |array|
 end
 
 start= "BNSOSBBKPCSCPKPOPNNK"
-# start= "NNCB"
 start= start.split("")
 start= start.zip(start[1..-1])
 count= 0
